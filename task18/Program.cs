47a5aea70854200ff33cd65bd50f2ff759e246a0
Mikheyev_Denis <mikheydd@mail.ru>
2022-11-18 20:43:50 +0300

    task18

--- task18/Program.cs
+++ task18/Program.cs
@@ -2,7 +2,7 @@
 // показывает диапазон возможных координат точек в этой четверти (x и y).
 var quarter = Convert.ToInt32(Console.ReadLine());
 
-switch(quarter)
+switch (quarter)
 {
     case 1:
         Console.WriteLine("x > 0 and y > 0");
